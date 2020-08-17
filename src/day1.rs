41adc627345edce8d40fdc3e7f6607b18eabcaa9
Brian Perrett <perrettbrian@gmail.com>
2020-08-16 21:35:00 -0600

    remove commented line
    day 1 part 2
    
    remove unecessary type declarations?

--- src/day1.rs
+++ src/day1.rs
@@ -5,18 +5,27 @@ pub fn main() {
     let f = File::open("./src/data/day1.txt").unwrap();
     let reader = BufReader::new(f);
 
-    let mut total: i32 = 0;
-    let mut x: i32;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in reader.lines() {
         let input = line.unwrap().parse::<i32>().unwrap();
-        x = find_fuel(input);
-        // println!("{}", x);
-        total += x;
+        part1 += find_fuel(input);
+        part2 += find_fuel_recursive(input);
     }
-    println!("total: {}", total);
+
+    println!("part1: {}", part1);
+    println!("part2: {}", part2);
 }
 
 fn find_fuel(mass: i32) -> i32 {
+    (mass / 3) - 2
+}
+
+fn find_fuel_recursive(mass: i32) -> i32 {
     let x = (mass / 3) - 2;
-    return x;
+    if x > 0 {
+        x + find_fuel_recursive(x)
+    } else {
+        0
+    }
 }
