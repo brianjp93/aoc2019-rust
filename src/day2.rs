a7a13e85e93a1a22074a30020bae6b71b44dbece
Brian Perrett <perrettbrian@gmail.com>
2020-08-16 23:13:08 -0600

    apply rustfmt

--- src/day2.rs
+++ src/day2.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
 pub fn main() {
-    let input = fs::read_to_string("./src/data/day2.txt")
-        .expect("Error");
-    let mut vec: Vec<i64> = input.trim()
+    let input = fs::read_to_string("./src/data/day2.txt").expect("Error");
+    let mut vec: Vec<i64> = input
+        .trim()
         .split(",")
         .map(|x| x.parse().unwrap())
         .collect();
