f380a29eaa93ac430d9be2af3a53f4f0fc7e8fe6
Brian Perrett <perrettbrian@gmail.com>
2020-08-20 23:26:25 -0600

    start using &mut to actually make my methods useful

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -5,36 +5,51 @@ pub fn main() {
     let mut comp = Computer::new(input.clone());
     comp.inputs.push(1);
     comp.run();
+    println!("Part 1: {}", comp.outputs.last().unwrap());
+
+    let mut comp = Computer::new(input.clone());
+    comp.inputs.push(5);
+    comp.run();
+    println!("Part 2: {}", comp.outputs.last().unwrap());
 }
 
 struct Computer {
     code: Vec<i64>,
     index: usize,
     inputs: Vec<i64>,
+    outputs: Vec<i64>,
 }
 
 impl Computer {
     fn new(code: Vec<i64>) -> Self {
-        let mut x = Computer { code, index: 0, inputs: Vec::<i64>::new() };
-        x.code.extend((1..1000).map(|_x| 0).collect::<Vec<i64>>());
+        let mut x = Computer {
+            code,
+            index: 0,
+            inputs: Vec::<i64>::new(),
+            outputs: Vec::<i64>::new(),
+        };
+        x.code.extend((1..1000).map(|_| 0).collect::<Vec<i64>>());
         x
     }
     fn run(&mut self) {
-        while self.get_opcode() != 99 {
-            let [ans, pos, new_index] = match self.get_opcode() {
+        loop {
+            match self.get_opcode() {
                 1 => self.add(),
                 2 => self.mul(),
                 3 => self.get_input(),
                 4 => self.output(),
+                5 => self.jump_if_true(),
+                6 => self.jump_if_false(),
+                7 => self.less_than(),
+                8 => self.equals(),
+                99 => {
+                    break;
+                }
                 _ => {
                     println!("wtf");
-                    break
-                },
+                    break;
+                }
             };
-            if self.get_opcode() != 4 {
-                self.code[pos as usize] = ans;
-            }
-            self.index = new_index as usize;
         }
     }
     fn get_opcode(&self) -> i64 {
@@ -42,7 +57,6 @@ impl Computer {
     }
     fn get_positions(&self) -> [i64; 3] {
         let opcode = self.code[self.index as usize];
-        // let e = &opcode % 100;
         let c = (opcode / 100) % 10;
         let b = (opcode / 1000) % 10;
         let a = (opcode / 10000) % 10;
@@ -56,24 +70,59 @@ impl Computer {
         }
         out
     }
-    fn get_input(&mut self) -> [i64; 3] {
+    fn less_than(&mut self) {
+        let [pos1, pos2, pos3] = self.get_positions();
+        if self.code[pos1 as usize] < self.code[pos2 as usize] {
+            self.code[pos3 as usize] = 1;
+        } else {
+            self.code[pos3 as usize] = 0;
+        }
+        self.index += 4;
+    }
+    fn equals(&mut self) {
+        let [pos1, pos2, pos3] = self.get_positions();
+        if self.code[pos1 as usize] == self.code[pos2 as usize] {
+            self.code[pos3 as usize] = 1;
+        } else {
+            self.code[pos3 as usize] = 0;
+        }
+        self.index += 4;
+    }
+    fn jump_if_true(&mut self) {
+        let [pos1, pos2, _] = self.get_positions();
+        if self.code[pos1 as usize] != 0 {
+            self.index = self.code[pos2 as usize] as usize;
+        } else {
+            self.index += 3;
+        }
+    }
+    fn jump_if_false(&mut self) {
+        let [pos1, pos2, _] = self.get_positions();
+        if self.code[pos1 as usize] == 0 {
+            self.index = self.code[pos2 as usize] as usize;
+        } else {
+            self.index += 3;
+        }
+    }
+    fn get_input(&mut self) {
         let [pos1, _, _] = self.get_positions();
         let val = self.inputs.pop().unwrap();
-        [val, pos1, self.index as i64 + 2]
+        self.code[pos1 as usize] = val;
+        self.index += 2;
     }
-    fn output(&self) -> [i64; 3] {
+    fn output(&mut self) {
         let [pos1, _, _] = self.get_positions();
-        println!("output: {}", self.code[pos1 as usize]);
-        [0, 0, self.index as i64 + 2]
+        self.outputs.push(self.code[pos1 as usize]);
+        self.index += 2;
     }
-    fn add(&self) -> [i64; 3] {
+    fn add(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        let ans = self.code[pos1 as usize] + self.code[pos2 as usize];
-        [ans, pos3, self.index as i64 + 4]
+        self.code[pos3 as usize] = self.code[pos1 as usize] + self.code[pos2 as usize];
+        self.index += 4;
     }
-    fn mul(&self) -> [i64; 3] {
+    fn mul(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        let ans = self.code[pos1 as usize] * self.code[pos2 as usize];
-        [ans, pos3, self.index as i64 + 4]
+        self.code[pos3 as usize] = self.code[pos1 as usize] * self.code[pos2 as usize];
+        self.index += 4;
     }
 }
