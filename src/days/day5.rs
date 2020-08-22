1354767f61f1ea73bee54ec7391520f36fbfb949
Brian Perrett <perrettbrian@gmail.com>
2020-08-22 12:56:09 -0600

    clean up day 5 some more

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -28,7 +28,7 @@ impl Computer {
             inputs: Vec::<i64>::new(),
             outputs: Vec::<i64>::new(),
         };
-        x.code.extend((1..1000).map(|_| 0).collect::<Vec<i64>>());
+        x.code.extend((1..100).map(|_| 0).collect::<Vec<i64>>());
         x
     }
     fn run(&mut self) {
@@ -53,53 +53,53 @@ impl Computer {
         }
     }
     fn get_opcode(&self) -> i64 {
-        self.code[self.index as usize] % 100
+        self.code[self.index] % 100
     }
-    fn get_positions(&self) -> [i64; 3] {
-        let opcode = self.code[self.index as usize];
+    fn get_positions(&self) -> [usize; 3] {
+        let opcode = self.code[self.index];
         let c = (opcode / 100) % 10;
         let b = (opcode / 1000) % 10;
         let a = (opcode / 10000) % 10;
-        let mut out = [0, 0, 0];
+        let mut out = [0 as usize, 0 as usize, 0 as usize];
         for (i, mode) in [c, b, a].iter().enumerate() {
             if *mode == 0 {
-                out[i] = self.code[self.index + i + 1];
+                out[i] = self.code[self.index + i + 1] as usize;
             } else {
-                out[i] = (self.index + i + 1) as i64;
+                out[i] = self.index + i + 1;
             }
         }
         out
     }
     fn less_than(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        if self.code[pos1 as usize] < self.code[pos2 as usize] {
-            self.code[pos3 as usize] = 1;
+        if self.code[pos1] < self.code[pos2] {
+            self.code[pos3] = 1;
         } else {
-            self.code[pos3 as usize] = 0;
+            self.code[pos3] = 0;
         }
         self.index += 4;
     }
     fn equals(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        if self.code[pos1 as usize] == self.code[pos2 as usize] {
-            self.code[pos3 as usize] = 1;
+        if self.code[pos1] == self.code[pos2] {
+            self.code[pos3] = 1;
         } else {
-            self.code[pos3 as usize] = 0;
+            self.code[pos3] = 0;
         }
         self.index += 4;
     }
     fn jump_if_true(&mut self) {
         let [pos1, pos2, _] = self.get_positions();
-        if self.code[pos1 as usize] != 0 {
-            self.index = self.code[pos2 as usize] as usize;
+        if self.code[pos1] != 0 {
+            self.index = self.code[pos2] as usize;
         } else {
             self.index += 3;
         }
     }
     fn jump_if_false(&mut self) {
         let [pos1, pos2, _] = self.get_positions();
-        if self.code[pos1 as usize] == 0 {
-            self.index = self.code[pos2 as usize] as usize;
+        if self.code[pos1] == 0 {
+            self.index = self.code[pos2] as usize;
         } else {
             self.index += 3;
         }
@@ -107,22 +107,22 @@ impl Computer {
     fn get_input(&mut self) {
         let [pos1, _, _] = self.get_positions();
         let val = self.inputs.pop().unwrap();
-        self.code[pos1 as usize] = val;
+        self.code[pos1] = val;
         self.index += 2;
     }
     fn output(&mut self) {
         let [pos1, _, _] = self.get_positions();
-        self.outputs.push(self.code[pos1 as usize]);
+        self.outputs.push(self.code[pos1]);
         self.index += 2;
     }
     fn add(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        self.code[pos3 as usize] = self.code[pos1 as usize] + self.code[pos2 as usize];
+        self.code[pos3] = self.code[pos1] + self.code[pos2];
         self.index += 4;
     }
     fn mul(&mut self) {
         let [pos1, pos2, pos3] = self.get_positions();
-        self.code[pos3 as usize] = self.code[pos1 as usize] * self.code[pos2 as usize];
+        self.code[pos3] = self.code[pos1] * self.code[pos2];
         self.index += 4;
     }
 }
