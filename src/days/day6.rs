341c164898efc8b56294f2e0b43b4f27fcc0bb43
Brian Perrett <perrettbrian@gmail.com>
2020-08-22 22:25:55 -0600

    start day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,15 @@
-pub fn main() {
+use std::fs;
+use std::collections::HashMap;
 
+
+pub fn main() {
+    println!("hello");
+    let data = fs::read_to_string("./src/data/day6.txt").expect("Error");
+    let data_list: Vec<_> = data.trim().split("\n").collect();
+    let mut data: HashMap<String, String> = HashMap::new();
+    for elt in data_list {
+        let parts = elt.split(")");
+        println!("{:?}", parts);
+    }
+    println!("{:?}", data);
 }
